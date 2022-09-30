65cae1c5be9a8df33b440e700fbc249f32e8918f
SamX23 <skalexsong@gmail.com>
2022-09-30 21:15:30 +0700

    Update : add footer (WIP)

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,23 @@ import { Box } from "@mui/material";
 
 const Footer = () => {
   return (
-    <Box component="footer" sx={{ bgcolor: "background.paper", py: 6 }}>
+    <Box
+      component="footer"
+      sx={{ display: "flex", bgcolor: "background.paper", py: 6 }}
+    >
+      <div>
+        <img src="#" alt="" />
+        <div>
+          <span>009-008-0045</span>
+          <span>www.wazabooking.com</span>
+        </div>
+        <div>
+          <span>Instagram</span>
+          <span>Facebook</span>
+          <span>Twitter</span>
+        </div>
+      </div>
+      <div>footer</div>
       <div>footer</div>
     </Box>
   );
