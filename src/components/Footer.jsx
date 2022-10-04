5d8b820a4f291705540b5f59bdc222af03804092
SamX23 <skalexsong@gmail.com>
2022-10-04 10:42:12 +0700

    Update : footer component

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,4 +1,12 @@
-import { Box, IconButton, Typography } from "@mui/material";
+import {
+  Box,
+  Button,
+  Container,
+  Grid,
+  IconButton,
+  TextField,
+  Typography,
+} from "@mui/material";
 import imgLogo from "../assets/logo.png";
 import InstagramIcon from "@mui/icons-material/Instagram";
 import FacebookIcon from "@mui/icons-material/Facebook";
@@ -6,76 +14,156 @@ import TwitterIcon from "@mui/icons-material/Twitter";
 
 const Footer = () => {
   return (
-    <Box
-      component="footer"
-      sx={{ display: "flex", bgColor: "secondary", py: 6 }}
-    >
-      <Box sx={{ display: "flex", flexDirection: "column" }}>
-        <img src={imgLogo} alt="logo" />
-        <div>
-          <Typography variant="h6" component="h5" color="text.secondary">
-            009-008-0045
-          </Typography>
-          <Typography variant="h6" component="h5" color="text.secondary">
-            www.wazabooking.com
-          </Typography>
-        </div>
-        <div>
-          <IconButton>
-            <InstagramIcon />
-          </IconButton>
-          <IconButton>
-            <TwitterIcon />
-          </IconButton>
-          <IconButton>
-            <FacebookIcon />
-          </IconButton>
-        </div>
-      </Box>
-      <Box
-        sx={{ textAlign: "center", display: "flex", flexDirection: "column" }}
-      >
-        <Typography variant="h5" component="h5">
-          Don't want to miss
-        </Typography>
-        <Typography variant="h5" component="h5">
-          Travel & Tour offers
-        </Typography>
-      </Box>
-      <Box sx={{ display: "flex", flexDirection: "column" }}>
-        <ul>
-          <li>
-            <Typography variant="h6" component="h5">
-              Smart Booking
-            </Typography>
-          </li>
-          <li>
-            <Typography variant="h6" component="h5" color="text.secondary">
-              Our Story
-            </Typography>
-          </li>
-          <li>
-            <Typography variant="h6" component="h5" color="text.secondary">
-              Our Story
-            </Typography>
-          </li>
-          <li>
-            <Typography variant="h6" component="h5" color="text.secondary">
-              Our Story
-            </Typography>
-          </li>
-          <li>
-            <Typography variant="h6" component="h5" color="text.secondary">
-              Our Story
-            </Typography>
-          </li>
-          <li>
-            <Typography variant="h6" component="h5" color="text.secondary">
-              Our Story
-            </Typography>
-          </li>
-        </ul>
-      </Box>
+    <Box component="footer" py={8} sx={{ bgcolor: "#F3F2EE" }}>
+      <Container maxWidth="xl">
+        <Grid container spacing={5} alignItems="center">
+          <Grid
+            item
+            md={3}
+            sx={{
+              display: "flex",
+              flexDirection: "column",
+              h: "100%",
+              justifyContent: "space-between",
+            }}
+          >
+            <img src={imgLogo} alt="logo" />
+
+            <Box
+              sx={{
+                display: "flex",
+                justifyContent: "space-between",
+                flexDirection: "column",
+              }}
+            >
+              <Typography
+                variant="h6"
+                component="h5"
+                color="text.secondary"
+                fontFamily="Times New Roman"
+              >
+                009-008-0045
+              </Typography>
+              <Typography
+                variant="h6"
+                component="h5"
+                color="text.secondary"
+                fontFamily="Times New Roman"
+              >
+                www.wazabooking.com
+              </Typography>
+            </Box>
+            <Box>
+              <IconButton>
+                <InstagramIcon />
+              </IconButton>
+              <IconButton>
+                <TwitterIcon />
+              </IconButton>
+              <IconButton>
+                <FacebookIcon />
+              </IconButton>
+            </Box>
+          </Grid>
+
+          <Grid
+            item
+            md={6}
+            height="100%"
+            sx={{
+              textAlign: "center",
+            }}
+          >
+            <Box>
+              <Typography
+                variant="h4"
+                component="h5"
+                fontFamily="Times New Roman"
+              >
+                Don't want to miss
+              </Typography>
+              <Typography
+                variant="h4"
+                component="h5"
+                fontFamily="Times New Roman"
+              >
+                Travel & Tour offers
+              </Typography>
+            </Box>
+            <Box
+              component="form"
+              noValidate
+              autoComplete="off"
+              sx={{
+                display: "flex",
+                gap: ".5rem",
+                mx: "auto",
+                mt: "1rem",
+                maxWidth: 500,
+              }}
+            >
+              <TextField
+                id="email"
+                label="Email"
+                variant="outlined"
+                type="email"
+                sx={{ width: "100%", bgcolor: "white" }}
+              />
+              <Button
+                variant="contained"
+                size="large"
+                color="success"
+                sx={{ width: "50%" }}
+              >
+                Sign Up
+              </Button>
+            </Box>
+          </Grid>
+
+          <Grid
+            item
+            md={3}
+            component="ul"
+            sx={{ m: 0, listStyle: "none", p: 0 }}
+          >
+            <Box component="li" sx={{ py: 0.5 }}>
+              <Typography variant="h6" component="h5">
+                Smart Booking
+              </Typography>
+            </Box>
+            <Box component="li" sx={{ py: 0.5 }}>
+              <Typography variant="h6" component="h5" color="text.secondary">
+                Our Story
+              </Typography>
+            </Box>
+            <Box component="li" sx={{ py: 0.5 }}>
+              <Typography variant="h6" component="h5" color="text.secondary">
+                Our Story
+              </Typography>
+            </Box>
+            <Box component="li" sx={{ py: 0.5 }}>
+              <Typography variant="h6" component="h5" color="text.secondary">
+                Photos
+              </Typography>
+            </Box>
+            <Box component="li" sx={{ py: 0.5 }}>
+              <Typography variant="h6" component="h5" color="text.secondary">
+                Review
+              </Typography>
+            </Box>
+            <Box component="li" sx={{ py: 0.5 }}>
+              <Typography variant="h6" component="h5" color="text.secondary">
+                News
+              </Typography>
+            </Box>
+            <Box component="li" sx={{ py: 0.5 }}>
+              <Typography variant="h6" component="h5" color="text.secondary">
+                Contact
+              </Typography>
+            </Box>
+          </Grid>
+        </Grid>
+      </Container>
     </Box>
   );
 };
